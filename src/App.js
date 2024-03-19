9cff35711400b6d9381bd03b3c1bbcf95934cf2c
Preethi-Eswar <164035506+Preethi-Eswar@users.noreply.github.com>
2024-03-19 21:18:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <h1>This  App.js file is pushed from loacl repository</h1>
+     <h1>Remote Repository</h1>
+    <p>A remote URL is Git's fancy way of saying "the place where your code is stored." That URL could be your repository on GitHub, or another user's fork, or even on a completely different server.</p>
+   <p>You can only push to two types of URL addresses:</p>
+    <ul>
+    <li>An HTTPS URL like https://github.com/user/repo.git</li>
+  <li>An SSH URL, like git@github.com:user/repo.git</li>
+    </ul>
+    <p>Git associates a remote URL with a name, and your default remote is usually called origin.</p>
+    
     </div>
   );
 }
